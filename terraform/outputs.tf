db7d9fff9d3eeb07e790e45caf8d828f5cfe996c
sajalmishra786 <sajalmishra2005@gmail.com>
2024-07-20 15:37:12 +0530

    errors corrected

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,10 @@
-
- output "cluster_name" {
+output "cluster_name" {
   description = "Amazon Web Service EKS Cluster Name"
   value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  description = "Endpoint for Amazon Web Service EKS "
+  description = "Endpoint for Amazon Web Service EKS"
   value       = module.eks.cluster_endpoint
 }
 
@@ -14,8 +13,7 @@ output "region" {
   value       = var.region
 }
 
-
 output "cluster_security_group_id" {
-  description = "Security group ID for the Amazon Web Service EKS Cluster "
+  description = "Security group ID for the Amazon Web Service EKS Cluster"
   value       = module.eks.cluster_security_group_id
 }
